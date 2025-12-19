5c89d10808115ac39d092a459711da0912ae254b
David Bryson <david@statichacks.org>
2025-12-18 16:34:29 -0800

    Fix navbar logo link to go to homepage without UTM tracking

--- packages/ui/src/nav/nav.tsx
+++ packages/ui/src/nav/nav.tsx
@@ -98,15 +98,7 @@ export function Nav({
           <MaxWidthWrapper className={cn("relative", maxWidthWrapperClassName)}>
             <div className="flex h-14 items-center justify-between">
               <div className="grow basis-0">
-                <Link
-                  className="block w-fit py-2 pr-2"
-                  href={createHref("/home", domain, {
-                    utm_source: "Custom Domain",
-                    utm_medium: "Navbar",
-                    utm_campaign: domain,
-                    utm_content: "Logo",
-                  })}
-                >
+                <Link className="block w-fit py-2 pr-2" href="/">
                   <NavWordmark />
                 </Link>
               </div>
